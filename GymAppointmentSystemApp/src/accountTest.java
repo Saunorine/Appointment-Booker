ef34e9f464384ef3a1e4da4c1e5d231bf94ecbd9
FerminGarduno <165826559+FerminGarduno@users.noreply.github.com>
2024-04-25 10:48:54 -0700

    Updated Accounts JUnit Test
    added tests to test addCourse and getCourse

--- GymAppointmentSystemApp/src/accountTest.java
+++ GymAppointmentSystemApp/src/accountTest.java
@@ -20,5 +20,32 @@ class accountTest {
 		assertEquals(testContactInfo, testAccount.getContact());
 	}
 
-	
+	@Test
+	void testAddCourseAndGetCourse()
+	{
+		//creating an Account Object to work with
+		Accounts joeAccount = new Accounts("Joe", "password321", "joe123@gmail.com");
+		
+		//checking the initial size of coursesEnrolledin
+		assertEquals(0, joeAccount.getCourses().size());	
+		
+		//Our test courses
+		String course1 = "personal training";
+		String course2 = "yoga class";
+		
+		//testing addCourses
+		joeAccount.addCourse(course1); 
+		joeAccount.addCourse(course2);
+		
+		//testing getCourses
+		//created ArrayList 
+		ArrayList<String> expectedCourses = new ArrayList<>(); 
+		expectedCourses.add(course1);
+		expectedCourses.add(course2);
+		//when we test it, joeAccount and expectedCourses should have the same course ID
+		assertEquals(expectedCourses, joeAccount.getCourses());
+		
+		
+		
+	}
 }
